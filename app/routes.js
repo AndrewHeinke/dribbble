2ebb259bacad9b78fa2b0d9f81570c7d577b2037
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-11-20 18:37:18 -0600

    setting up first project demo

--- app/routes.js
+++ app/routes.js
@@ -2,4 +2,7 @@ module.exports = function(app) {
   app.get('/', function(req, res) {
     res.render('pages/index.ejs');
   });
+  app.get('/first', function(req, res) {
+    res.render('pages/first.ejs');
+  })
 };
